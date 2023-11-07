5c15cd200b4a9179834916f79e7cce998a3ad3f8
sarah9548 <150169508+sarah9548@users.noreply.github.com>
2023-11-07 11:21:54 -0500

    First Upload

---
+++ P1LAB1_HousseinSarah.py
@@ -0,0 +1,9 @@
+#Sarah Houssein
+#11/7/2023
+#Basic Input/Output
+
+#Get input from user
+name = input ("Please enter your name:")
+
+#Display output to user
+print("Hello",name, "welcome to CTI-110")

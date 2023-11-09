1da182fadb75f18970ad25490af4158e7226e867
sarah9548 <150169508+sarah9548@users.noreply.github.com>
2023-11-09 11:08:10 -0500

    Add files via upload

---
+++ P1HW1_HousseinSarah.py
@@ -0,0 +1,20 @@
+
+#Sarah Houssein
+#11/9/2023
+#Using integers and math expressions
+
+
+#Get input from user
+number1 = int(input("Enter integer: "))
+
+#Display output
+print ("You entered",number1)
+print (number1, "squared is" , number1**2)
+print (number1, "cubed is" , number1**3, "!!")
+
+#Get input from user
+number2 = int(input("Enter another integer: "))
+
+#Display output
+print (number1, "+", number2, "is", number1 + number2)
+print (number1, "*", number2, "is", number1 * number2)
